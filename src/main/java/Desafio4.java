42a7e19570aae6d9a6332d0d73fca20b5cc4a3a9
Marcos V <usmarcos@yahoo.com.br>
2022-04-04 17:56:05 -0300

    Resoluções dos desafios do bootcamp GFT QA

---
+++ src/main/java/Desafio4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Desafio4 {
+
+    //    Desafio: Ler um número inteiro N e calcular todos os seus divisores.
+    //   Entrada: O arquivo de entrada contém um valor inteiro.
+    //    Saída: Escreva todos os divisores positivos de N, um valor por linha.
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+        for (int i = 1; i<=n ; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
